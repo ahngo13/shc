c4979c7bafa399eab515a7c50fffd20684868bb5
hamletshu <ahngo13@naver.com>
2020-05-01 18:50:50 -0700

    update

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 const express=require("express");
 const path=require("path");
-const app=express();
-
-const contactFormService=require('./service/contactFormService');
 const medicalFormService=require('./service/medicalFormService');
-
-app.use(express.static(path.join(__dirname,"/public")));
-app.use(express.json());
-
 var session=require('express-session');
 var cookieParser = require('cookie-parser');
+
+const app=express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser('!@#$%^&*()전은수는예쁘다'));
@@ -26,69 +21,70 @@ app.use(session({
   }));
 
 
-app.post('/contactForm', function (req, res) {
-    
+const contactFormService=require('./service/contactFormService');
+
+app.use(express.static(path.join(__dirname,"/public")));
+app.use(express.json());
+
+app.post('/contactForm', function (req, res) {   
 
     var name=req.body.name;
     var id=req.body.id;
-    var pw=req.body.pw;    var message=req.body.message;
+    var pw=req.body.pw;    
+    var message=req.body.message;
     console.log(name,id,pw,message);
     if(name && id && pw){
-        contactFormService.contactFormInsertOne(res,name,id,pw,message);        
+        contactFormService.contactFormInsertOne(res,name,id,pw,message);  
+           
     }else{
         res.send('Failure');
     }
   });
 
 
-  
-
-  app.post('/medicalForm', function (req, res) {
-      
-      var name=req.body.name;
-      var ssn=req.body.ssn;
-      var addr=req.body.addr;
-      var email=req.body.email;
-      var visitDate=req.body.visitDate;
-      var desease=req.body.desease;
-      var deseaseCode=req.body.deseaseCode;
-      var content=req.body.content;
-      console.log(name,ssn,addr,email,visitDate,desease,deseaseCode,content);
-      if(name && ssn){
-          medicalFormService.medicalFormInsertOne(name,ssn,addr,email,visitDate,desease,deseaseCode,content);
-          res.send('진료 확인서 저장 완료');
-      }else{
-          res.send('Failure');
-      }
-  
-    });
-  
-    app.get('/medical_report_req', function (req, res) {    
-        console.log(req.session);
-        if(req.session.loginedID){
-          res.sendFile(path.join(__dirname+'/public/html/medical_report.html'));
-        }else{
-          res.sendFile(path.join(__dirname+'/public/html/login.html'));
-        }
-      });
-
-      app.post('/login', function (req, res) {    
-        var id=req.body.id;
-        var pw=req.body.pw;   
-        console.log(id,pw);
-        if(id && pw){
-          contactFormService.login(req,res,id,pw);        
-        }else{
-            res.send('id와 pw를 입력하세요');
-        }
-      });
+app.post('/medicalReportForm', function (req, res) {
     
+    var name=req.body.name;
+    var ssn=req.body.ssn;
+    var addr=req.body.addr;
+    var email=req.body.email;
+    var visitDate=req.body.dday;
+    var desease=req.body.point;
+    var deseaseCode=req.body.code;
+    var content=req.body.desc;
+    var docterName=req.session.docterName;
+    var docterNo=req.session.docterNo;
+    console.log(name,ssn,addr,email,visitDate,desease,deseaseCode,content,docterName,docterNo);
+    if(name && ssn && docterName && docterNo){
+        medicalFormService.medicalFormInsertOne(name,ssn,addr,email,visitDate,desease,deseaseCode,content,docterName,docterNo);
+        res.send('진료 확인서 two phase 저장 완료');
+    }else{
+        res.send('Failure');
+    }
 
-      
-    
+  });
+
+app.get('/medical_report_req', function (req, res) {    
+    console.log(req.session);
+    if(req.session.loginedID){
+      res.sendFile(path.join(__dirname+'/public/html/medical_report.html'));
+    }else{
+      res.sendFile(path.join(__dirname+'/public/html/login.html'));
+    }
+  });
+
+app.post('/login', function (req, res) {    
+    var id=req.body.id;
+    var pw=req.body.pw;   
+    console.log(id,pw);
+    if(id && pw){
+      contactFormService.login(req,res,id,pw);        
+    }else{
+        res.send('id와 pw를 입력하세요');
+    }
+  });
 
 
 app.listen(7777,function(){
     console.log("7777 server ready...");
 });
-
--- service/contactFormService.js
+++ service/contactFormService.js
@@ -2,67 +2,36 @@ const MongoClient=require('mongodb').MongoClient;
 const url='mongodb://localhost:27017/SmartHC';
 
 module.exports={
-
     login:function(req,res,id,pw){
-            MongoClient.connect(url, function(err,db){
-                if(err){
-                    console.log(err);
-                }else{                
-                    db = db.db('SmartHC'); /*database명을 다시 한번 명시했다. 이거 안 하면 에러남*/
-                    console.log('db connected...ok');
-                    db.collection('contactForm').findOne(
-                        {                        
-                            "id":id,
-                            "pw":pw
-                        },
-                        (err, result)=>{
-                            if(err){
-                                console.log(err);
-                            }else{
-                                console.log('로그인 ok');
-                                console.log(result);
-                                req.session.loginedID=id;
-                                res.send(result.name+'님 로그인되셨습니다');
-                                
-                            }
-                            
-                        }
-                    );
-                    
-                }//end else
-                
-            });
-        },
-    
-    medicalFormInsertOne: function(name,ssn,addr,email,visitDate,desease,deseaseCode,content){
         MongoClient.connect(url, function(err,db){
             if(err){
                 console.log(err);
             }else{                
                 db = db.db('SmartHC'); /*database명을 다시 한번 명시했다. 이거 안 하면 에러남*/
                 console.log('db connected...ok');
-                db.collection('medicalForm').insertOne(
-                    {
-                        "name":name,
-                        "ssn":ssn,
-                        "addr":addr,
-                        "email":email,
-                        "visitDate":visitDate,
-                        "desease":desease,
-                        "deseaseCode":deseaseCode,
-                        "content":content
+                db.collection('contactForm').findOne(
+                    {                        
+                        "id":id,
+                        "pw":pw
                     },
                     (err, result)=>{
                         if(err){
                             console.log(err);
                         }else{
-                            console.log('진료확인서 저장됨\n');
-                            console.log(result);
+                            console.log('로그인 ok');
+                            console.log("==========>",result.name,":",result._id);
+                            req.session.loginedID=id;
+		       req.session.docterName=result.name;
+     		       req.session.docterNo=result._id;
+                            res.send(result.name+'님 로그인되셨습니다');
+                            
                         }
                         
                     }
                 );
-            }
+                
+            }//end else
+            
         });
     },
 
@@ -94,5 +63,4 @@ module.exports={
             }
         });
     }
-
 }
